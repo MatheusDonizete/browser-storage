2d0e351ffd06dd9e804edbed6e93d6f376049bc4
Matheus Donizete <matheus@strikexii.com>
2017-03-10 15:41:09 -0300

    insert data to IndexedDB
    Signed-off-by: Matheus Donizete <matheus@strikexii.com>

--- indexedDB.js
+++ indexedDB.js
@@ -3,17 +3,46 @@
 const entities = {};
 let db = null;
 
+const saveToEntity = e => {
+    entities[e.name] = { indexes: e.indexes };
+    entities[e.name].store = inMemory(e);
+};
+
+const inMemory = store => {
+    return {
+        put: (data) => {
+            const tx = db.transaction(store.name, "readwrite");
+            const storeObject = tx.objectStore(store.name);
+            storeObject.put(data);
+            return new Promise(resolve => {
+                tx.oncomplete = function() {
+                    resolve();
+                };
+            });
+        },
+        find: ({ field = '', value = '' }) => {
+            return field;
+        }
+    };
+};
+
+const applyToObjectOrArray = (data, fct) => {
+    if (data instanceof Array) {
+        data.forEach(fct);
+    } else {
+        fct(data);
+    }
+};
+
 const createStore = store => {
     const storeObject = db.createObjectStore(store.name, { keyPath: 'uuid' });
     store.indexes.forEach(index => {
-        storeObject.createIndex(index.name, index.field, index.options);
+        storeObject.createIndex(`by_${index.field}`, index.field, index.options);
     });
-
-    return storeObject;
 };
 
-const bindRequest = (req, stores) => {
-    const promise = new Promise((resolve, reject) => {
+const bindRequest = (req) => {
+    return new Promise((resolve, reject) => {
         req.onsuccess = () => {
             db = req.result;
             resolve();
@@ -21,25 +50,18 @@ const bindRequest = (req, stores) => {
 
         req.onerror = () => reject(req.error);
     }).catch(err => console.error('Something went wrong:', err));
-
-    promise.then(() => {
-        const save = e => {
-            entities[e.name] = { indexes: e.indexes };
-            entities[e.name].store = createStore(e);
-        };
-        req.onupgradeneeded = () => {
-            if (stores instanceof Array) {
-                stores.forEach(save);
-            } else {
-                save(stores);
-            }
-        };
-    });
 };
 
-const createBrowserDB = (database, stores) => {
-    const request = indexedDB.open(database);
-    bindRequest(request, stores);
+const createBrowserDB = ({ database, stores, version = 1 }) => {
+    const req = indexedDB.open(database, version);
+    req.onupgradeneeded = () => {
+        db = req.result;
+        applyToObjectOrArray(stores, createStore);
+        db.close();
+    };
+
+    applyToObjectOrArray(stores, saveToEntity);
+    return bindRequest(req, stores).then(() => Promise.resolve(entities));
 };
 
 export { createBrowserDB };
